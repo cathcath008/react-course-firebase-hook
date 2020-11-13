8bcff30c1c5d2900ab49b798300401923a46ad62
cathcath008 <mircelia008@gmail.com>
2020-11-14 00:10:32 +0700

    implementing-pagination

--- src/components/newsComponent/index.js
+++ src/components/newsComponent/index.js
@@ -3,18 +3,22 @@ import React from 'react';
 const News = (props) => {
   const { data } = props;
   return (
-    <div className="card">
-      <div className="card-header">{data.date}</div>
-      <div className="card-body">
-        {data.activity.map((act) => (
-          <>
-            <h5 className="card-title">{act.title}</h5>
-            <p className="card-text">{act.desc}</p>
-            <a href={act.url} className="btn btn-warning">
-              Go somewhere
-            </a>
-          </>
-        ))}
+    <div className="container">
+      <div className="card">
+        <div className="card-header">{data.date}</div>
+        <div className="card-body">
+          {data.activity.map((act) => {
+ return (
+   <>
+     <h5 className="card-title">{act.title}</h5>
+     <p className="card-text">{act.desc}</p>
+     <a href={act.url} className="btn btn-warning">
+       More info...
+     </a>
+   </>
+          );
+})}
+        </div>
       </div>
     </div>
     // <>
